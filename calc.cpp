3b2acb3f82caae38f17067c540558231a48ce12e
Janno <Janno>
2019-03-29 11:20:35 +0800

    calc

--- calc.cpp
+++ calc.cpp
@@ -1,16 +1,11 @@
-/*
-Name: CALCULATOR v1.0
-Author: Aldo Ziflaj
-Date: 01-01-13 12:01
-Description: A simple calculator that can add, subtract multiply, divide, 
-raise to n-th integer power and calculate the factorial
-*/
+//
+// This is a basic calculator implementation
+//
 
 #include <iostream>
 #include <cstdio>
 #include <cmath>
 #include <cstring>
-using namespace std;
 
 // Creating a branching feature
 // Creating a branching feature this is v3
@@ -27,44 +22,39 @@ int main ()
 	double a,b,r,memory=0;
 	char op, choice[10];
 
-	cout<<"+ to add, - to subtract, * to multiply, / to divide, \
-	^ to power, ! to factorial\n\n";
-	start: // THE BEGINING
+	std::cout << "+ to add\n- to subtract\n* to multiply\n/ to divide\n^ to power\n! to factorial\n\n";
+	start:
 
-	cin>>a; // 1ST NUMBER
-	reused: //
-	cin>>op; // OPERATOR
-	// WHAT TO DO
+	std::cin >> a;
+	reused:
+	std::cin >> op;
+	
 	if (op=='!') r=factorial(a); 
 	else
 	{
-		cin>>b;
+		std::cin>>b;
 		if (op=='+') r=a+b;
 		if (op=='-') r=a-b;
 		if (op=='*') r=a*b;
 		if (op=='/') r=a/b;
 		if (op=='^') r=pow(a,b);
 	}
-	// COUT THE RESULT
-	cout<<"="<<r<<endl;
+	
+	std::cout << "=" << r << std::endl;
 
-	// CREATING THE MEMORY
-	cout<<"\nType 'mi' to insert the number into memory, or 'mc' to clear memory\n";
-	cin>>choice;
-	if (!strcmp(choice,"mi")) memory=r; // INSERT THE MEMORY
-	else if (!strcmp(choice,"mc")) memory=0; // CLEAR THE MEMORY
-	else cout<<"command unknown, program will go on\n"; // DEFAULT
+	std::cout << "\nType 'mi' to insert the number into memory, or\n     'mc' to clear memory\n";
+	std::cin >> choice;
+	if (!strcmp(choice,"mi")) memory=r;
+	else if (!strcmp(choice,"mc")) memory=0;
+	else std::cout << "command unknown, program will go on\n";
 
-	// CREATING THE BASE FOR THE LOOP
-	cout<<"\nType 'restart' to start again from the beggining, \
-	'reuse' to use the result, \n'mr' to reuse the number \
-	in the memory, or 'quit' to quit: ";
-	cin>>choice; // TELL THE PROGRAM WHAT TO DO
+	std::cout << "\nType 'restart' to start again\n     'reuse' to use the result\n     'mr' to reuse the number in the memory, or \n     'quit' to quit: ";
+	std::cin >> choice;
 	if (!strcmp(choice,"restart")) goto start; // START FROM THE BEGINING
-	if (!strcmp(choice,"reuse")) { a=r; cout<<a; } // USE THE RESULT
+	if (!strcmp(choice,"reuse")) { a=r; std::cout << a; } // USE THE RESULT
 	if (!strcmp(choice,"reuse")) goto reused; // START WITH THE RESULT
 	if (!strcmp(choice,"quit")) goto end; // QUIT THE PROGRAM
-	if (!strcmp(choice,"mr")) { a=memory; cout<<a; } // USE THE MEMORY
+	if (!strcmp(choice,"mr")) { a=memory; std::cout << a; } // USE THE MEMORY
 	if (!strcmp(choice,"mr")) goto reused; // START WITH THE MEMORY
 	getchar();
 	end: // TO QUIT THE PROGRAM
